d9fd8ff4dab4c900f86fa812dd62e5685691f596
Tranquy20 <tquy5453@gmail.com>
2025-05-23 11:55:18 +0700

    QlyKho

---
+++ QlyKho/LOPDUNGCHUNG.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QlyKho
+{
+    class LOPDUNGCHUNG
+    {
+        SqlConnection conn;
+        public LOPDUNGCHUNG()
+        {
+            string chuoikn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""D:\CMU-CS 464 HIS\QlyKho\QlyKho\KHO.mdf"";Integrated Security=True";
+            conn = new SqlConnection(chuoikn);
+        }
+        public int ThemSuaXoa(string sql)
+        {
+            SqlCommand comm = new SqlCommand(sql, conn);
+            conn.Open();
+            int kq = comm.ExecuteNonQuery();
+            conn.Close();
+            return kq;
+        }
+        public object LayDuLieu(string sql)
+        {
+            SqlCommand comm = new SqlCommand(sql, conn);
+            conn.Open();
+            object kq = comm.ExecuteScalar();
+            conn.Close();
+            return kq;
+        }
+        public DataTable LoadDL(string sql)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+    }
+}
